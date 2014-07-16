f88e200c6f9d3688cfc2a8d1f900d5585ccdd60b
igorlealantunes <igorlealantunes@gmail.com>
2014-07-15 21:51:27 -0300

    g

--- LP3_ProjetoFinal_SJF/src/lp3/model/Cliente.java
+++ LP3_ProjetoFinal_SJF/src/lp3/model/Cliente.java
@@ -18,15 +18,24 @@ public class Cliente {
 		
 	}
 
+	
+	
 	public String getUsuario() {
 		return usuario;
 	}
 
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
 
 	public String getSenha() {
 		return senha;
 	}
-	
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
 	public String login() {
 		String action = null;
         
